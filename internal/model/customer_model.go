9bbe4cd1e622a04679d2d0959e136457cca06a5f
bwafi <syahronibwf@gmail.com>
2024-11-30 15:39:45 +0700

    refactor: change id to UUID

---
+++ internal/model/customer_model.go
@@ -0,0 +1,12 @@
+package model
+
+type CustomerResponse struct {
+	ID           string `json:"id,omitempty"`
+	EmailAddress string `json:"email_address,omitempty"`
+	PhoneNumber  string `json:"phone_number,omitempty"`
+	Token        string `json:"token,omitempty"`
+	CreatedAt    int64  `json:"created_at,omitempty"`
+	UpdatedAt    int64  `json:"updated_at,omitempty"`
+}
+
+type RegisterCustomerRequest struct{}
