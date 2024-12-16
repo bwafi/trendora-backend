2d62df8f54a73c46c93dc175157f21f2dee6568a
bwafi <syahronibwf@gmail.com>
2024-12-16 15:46:24 +0700

    chore: Fix type and add entity for address

--- internal/entity/customer_address.go
+++ internal/entity/customer_address.go
@@ -6,12 +6,20 @@ import (
 )
 
 type CustomerAddresses struct {
-	ID        string         `gorm:"column:id;type:uuid;default:uuid_generate_v4()"`
-	UserId    string         `gorm:"column:user_id"`
-	CreatedAt int64          `gorm:"column:created_at;autoCreateTime:milli"`
-	UpdatedAt int64          `gorm:"column:updated_at;autoCreateTime:milli;autoUpdateTime:milli"`
-	DeletedAt gorm.DeletedAt `gorm:"column:deleted_at;index"`
-	Customers Customers      `gorm:"foreignKey:user_id;references:id"`
+	ID            string         `gorm:"column:id;type:uuid;default:uuid_generate_v4()"`
+	CustomerID    string         `gorm:"column:customer_id;type:uuid;not null" json:"customer_id"`
+	RecipientName string         `gorm:"column:recipient_name;type:varchar(100);not null" json:"recipient_name"`
+	PhoneNumber   string         `gorm:"column:phone_number;type:varchar(20)" json:"phone_number"`
+	AddressType   string         `gorm:"column:address_type;type:varchar(10);not null" json:"address_type"`
+	City          string         `gorm:"column:city;type:varchar(100);not null" json:"city"`
+	Province      string         `gorm:"column:province;type:varchar(100);not null" json:"province"`
+	SubDistrict   string         `gorm:"column:sub_district;type:varchar(100)" json:"sub_district"`
+	PostalCode    string         `gorm:"column:postal_code;type:varchar(10)" json:"postal_code"`
+	CourierNotes  string         `gorm:"column:courier_notes;type:text" json:"courier_notes"`
+	CreatedAt     int64          `gorm:"column:created_at;autoCreateTime:milli"`
+	UpdatedAt     int64          `gorm:"column:updated_at;autoCreateTime:milli;autoUpdateTime:milli"`
+	DeletedAt     gorm.DeletedAt `gorm:"column:deleted_at;index"`
+	Customers     Customers      `gorm:"foreignKey:customer_id;references:id"`
 }
 
 func (c *CustomerAddresses) TableName() string {
--- internal/entity/customers.go
+++ internal/entity/customers.go
@@ -8,8 +8,8 @@ import (
 type Customers struct {
 	ID                string              `gorm:"column:id;type:uuid;default:uuid_generate_v4()"`
 	EmailAddress      *string             `gorm:"column:email_address;unique"`
-	PhoneNumber       *string             `gorm:"column:phone_number;unique"`
-	Name              string              `gorm:"column:name;not null"`
+	PhoneNumber       *string             `gorm:"column:phone_number;type:varchar(20);unique"`
+	Name              string              `gorm:"column:name;type:varchar(50);not null"`
 	Gender            bool                `gorm:"column:gender"`
 	DateOfBirth       int64               `gorm:"column:date_of_birth"`
 	Password          string              `gorm:"column:password;not null"`
@@ -17,7 +17,7 @@ type Customers struct {
 	CreatedAt         int64               `gorm:"column:created_at;autoCreateTime:milli"`
 	UpdatedAt         int64               `gorm:"column:updated_at;autoCreateTime:milli;autoUpdateTime:milli"`
 	DeletedAt         gorm.DeletedAt      `gorm:"column:deleted_at;index"`
-	CustomerAddresses []CustomerAddresses `gorm:"foreignKey:user_id;references:id"`
+	CustomerAddresses []CustomerAddresses `gorm:"foreignKey:customer_id;references:id"`
 }
 
 func (c *Customers) TableName() string {
