32aa7449996fc3bf6361419e28efa57e3182cefd
bwafi <syahronibwf@gmail.com>
2024-12-16 15:47:47 +0700

    fix: Fix constraint not null customer_addresses table

--- db/migrations/20241214114053_create_table_customers_addresses.up.sql
+++ db/migrations/20241214114053_create_table_customers_addresses.up.sql
@@ -1,18 +1,16 @@
-create table customer_addresses (
+CREATE TABLE customer_addresses (
   id VARCHAR(255) PRIMARY KEY,
-  user_id VARCHAR(255),
   customer_id VARCHAR(255) NOT NULL,
-  recipient_name VARCHAR(255) NOT NULL,
-  phone_number varchar(20) NOT NULL,
-  address_type varchar[10] not null,
-  city varchar(100) NOT NULL,
-  province varchar(100) NOT NULL,
-  sub_district varchar(100),
-  postal_code varchar(10),
-  courier_notes text,
+  recipient_name VARCHAR(100) NOT NULL,
+  phone_number VARCHAR(20) NOT NULL,
+  address_type VARCHAR(10) NOT NULL,
+  city VARCHAR(100) NOT NULL,
+  province VARCHAR(100) NOT NULL,
+  sub_district VARCHAR(100) NOT NULL,
+  postal_code VARCHAR(10) NOT NULL,
+  courier_notes TEXT,
   created_at BIGINT NOT NULL,
   updated_at BIGINT NOT NULL,
   deleted_at TIMESTAMP,
-  FOREIGN KEY(user_id) REFERENCES customers(id)
+  FOREIGN KEY (customer_id) REFERENCES customers (id)
 );
-
