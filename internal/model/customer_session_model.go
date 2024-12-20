6548f74029f8b4f3d70814f490bfa865968f6bcb
bwafi <syahronibwf@gmail.com>
2024-12-20 15:43:32 +0700

    fix: Fix json tag in 'customer_session_model'

--- internal/model/customer_session_model.go
+++ internal/model/customer_session_model.go
@@ -5,7 +5,7 @@ import "time"
 type CustomerSessionRequest struct {
 	ID           string    `json:"id,omitempty"`
 	CustomerID   string    `json:"customer_id"`
-	RefreshToken string    `json:refresh_token`
-	IsRevoked    string    `json:"column:is_revoked`
-	ExpiresAt    time.Time `json:"column:expires_at"`
+	RefreshToken string    `json:"refresh_token"`
+	IsRevoked    string    `json:"is_revoked"`
+	ExpiresAt    time.Time `json:"expires_at"`
 }
