3b996b5c2d520893eb9178d94ed5d08a10f852c0
bwafi <syahronibwf@gmail.com>
2025-01-05 01:12:11 +0700

    fix: Add deleted_at and gender in migration poducts

--- db/migrations/20241225223119_create_table_products.up.sql
+++ db/migrations/20241225223119_create_table_products.up.sql
@@ -3,6 +3,7 @@ CREATE TABLE products (
   style_code VARCHAR(50) NOT NULL,
   name VARCHAR(255) NOT NULL,
   description TEXT,
+  gender VARCHAR(10),
   category_id VARCHAR(255),
   sub_category_id VARCHAR(255),
   base_price DECIMAL(10,2),
@@ -10,6 +11,7 @@ CREATE TABLE products (
   release_date BIGINT,
   created_at BIGINT,
   updated_at BIGINT,
+  deleted_at TIMESTAMP,
   FOREIGN KEY(category_id) REFERENCES categories(id),
   FOREIGN KEY(sub_category_id) REFERENCES categories(id)
 )
