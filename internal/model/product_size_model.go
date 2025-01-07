b65596e63c881486ad58391a34008c652177bc44
bwafi <syahronibwf@gmail.com>
2025-01-07 21:22:03 +0700

    fix: Fix form tag struct for prduct size

--- internal/model/product_size_model.go
+++ internal/model/product_size_model.go
@@ -12,10 +12,10 @@ type ProductSizeResponse struct {
 }
 
 type ProductSizeRequest struct {
-	VariantId     string  `json:"variant_id"`
-	SKU           string  `json:"sku"`
-	Size          string  `json:"size"`
-	Discount      float32 `json:"discount"`
-	Price         float32 `json:"price"`
-	StockQuantity int     `json:"stock_quantity"`
+	VariantId     string  `json:"variant_id" form:"variant_id"`
+	SKU           string  `json:"sku" form:"sku"`
+	Size          string  `json:"size" form:"size"`
+	Discount      float32 `json:"discount" form:"discount"`
+	Price         float32 `json:"price" form:"price"`
+	StockQuantity int     `json:"stock_quantity" form:"stock_quantity"`
 }
