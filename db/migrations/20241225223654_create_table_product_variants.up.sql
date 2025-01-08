a458a0295e35d1cebb3db9e71a20e4668608dea7
bwafi <syahronibwf@gmail.com>
2025-01-08 11:30:53 +0700

    fix: add missing column on migration product size and product variiant

--- db/migrations/20241225223654_create_table_product_variants.up.sql
+++ db/migrations/20241225223654_create_table_product_variants.up.sql
@@ -1,6 +1,7 @@
 CREATE TABLE product_variants (
   id VARCHAR(255) PRIMARY KEY,
   product_id VARCHAR(255),
+  sku VARCHAR(100) NOT NULL UNIQUE,
   color_name VARCHAR(50),
   weight DECIMAL(8,2),
   is_available BOOLEAN,
--- db/migrations/20250106220124_create_table_product_sizes.up.sql
+++ db/migrations/20250106220124_create_table_product_sizes.up.sql
@@ -6,7 +6,6 @@ CREATE TABLE product_sizes (
     price DECIMAL(10,2) NOT NULL,
     discount DECIMAL(5,2) DEFAULT 0.00,
     stock_quantity INTEGER NOT NULL DEFAULT 0,
-    created_at BIGINT NOT NULL,
-    updated_at BIGINT NOT NULL,
+    is_available BOOLEAN,
     CONSTRAINT fk_variant FOREIGN KEY (variant_id) REFERENCES product_variants(id) ON DELETE CASCADE
 );
