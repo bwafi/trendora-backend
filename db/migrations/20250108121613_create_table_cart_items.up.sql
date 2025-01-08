df15e2b5c2f381f40edf7aaa2dacec60d15dd908
bwafi <syahronibwf@gmail.com>
2025-01-08 19:27:12 +0700

    chore: Add miration for cart_items

---
+++ db/migrations/20250108121613_create_table_cart_items.up.sql
@@ -0,0 +1,12 @@
+CREATE TABLE cart_items (
+  id VARCHAR(255) PRIMARY KEY,
+  customer_id VARCHAR(255) NOT NULL,
+  product_id VARCHAR(255) NOT NULL,
+  variant_id VARCHAR(255) NOT NULL,
+  quantity INTEGER NOT NULL,
+  created_at BIGINT,
+  updated_at BIGINT,
+  FOREIGN KEY(customer_id) REFERENCES customers(id),
+  FOREIGN KEY(product_id) REFERENCES products(id),
+  FOREIGN KEY(variant_id) REFERENCES product_variants(id)
+);
