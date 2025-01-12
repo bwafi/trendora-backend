d010a8433b8bdd971738711443ba0349fb1e9bdf
bwafi <syahronibwf@gmail.com>
2025-01-12 13:27:19 +0700

    chore: Add omitempty on `ProductResponse`

--- internal/model/product_model.go
+++ internal/model/product_model.go
@@ -2,20 +2,20 @@ package model
 
 type ProductResponse struct {
 	ID             string                   `json:"id,omitempty"`
-	StyleCode      string                   `json:"style_code"`
-	Name           string                   `json:"name"`
-	Description    string                   `json:"description"`
-	Gender         string                   `json:"gender"`
-	CategoryId     string                   `json:"category_id"`
-	SubCategoryId  string                   `json:"sub_category_id"`
-	BasePrice      float32                  `json:"base_price"`
-	IsVisible      bool                     `json:"is_visible"`
-	ReleaseDate    int64                    `json:"release_date"`
-	Category       CategoryResponse         `json:"category"`
-	ProductVariant []ProductVariantResponse `json:"variant"`
-	ProductImages  []ImageResponse
-	CreatedAt      int64 `json:"created_at"`
-	UpdatedAt      int64 `json:"updated_at"`
+	StyleCode      string                   `json:"style_code,omitempty"`
+	Name           string                   `json:"name,omitempty"`
+	Description    string                   `json:"description,omitempty"`
+	Gender         string                   `json:"gender,omitempty"`
+	CategoryId     string                   `json:"category_id,omitempty"`
+	SubCategoryId  string                   `json:"sub_category_id,omitempty"`
+	BasePrice      float32                  `json:"base_price,omitempty"`
+	IsVisible      bool                     `json:"is_visible,omitempty"`
+	ReleaseDate    int64                    `json:"release_date,omitempty"`
+	Category       CategoryResponse         `json:"category,omitempty"`
+	ProductVariant []ProductVariantResponse `json:"variant,omitempty"`
+	ProductImages  []ImageResponse          `json:"product_images,omitempty"`
+	CreatedAt      int64                    `json:"created_at,omitempty"`
+	UpdatedAt      int64                    `json:"updated_at,omitempty"`
 }
 
 type CreateProductRequest struct {
